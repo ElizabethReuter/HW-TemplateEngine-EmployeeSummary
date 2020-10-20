09cb32d6082e87e26392d0aa92acccefc2285d0d
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-10-20 15:00:52 -0500

    started classes for each role

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+
+    }
+}
+
+var ob = new Employee("Tom", 1, "test@test.com");
+var ob2 = new Employee("Dave", 2, "test2@test.com");
+
+console.log(ob, ob2);
+ob.getName();
+
+module.exports = Employee; 
